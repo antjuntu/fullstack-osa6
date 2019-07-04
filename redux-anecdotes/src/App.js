081beddefaccebd817f0bdc3e9136e28cfa95f5c
antjuntu <antjuntu@gmail.com>
2019-07-04 19:21:14 +0300

    6.18 anekdootit ja backend, step4

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -4,14 +4,11 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
-import anecdoteService from './services/anecdotes'
-import { initialAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = (props) => {
   useEffect(() => {
-    anecdoteService
-      .getAll()
-      .then(anecdotes => props.initialAnecdotes(anecdotes))
+    props.initializeAnecdotes()
   }, [props])
 
   return (
@@ -25,4 +22,4 @@ const App = (props) => {
   )
 }
 
-export default connect(null, { initialAnecdotes })(App)
+export default connect(null, { initializeAnecdotes })(App)
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import anecdoteService from '../services/anecdotes'
+
 const reducer = (state = [], action) => {
   switch(action.type) {
     case 'VOTE':
@@ -33,10 +35,13 @@ export const createAnecdote = (data) => {
   }
 }
 
-export const initialAnecdotes = (anecdotes) => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes
+    })
   }
 }
 
